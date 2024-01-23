db5409e514b59fc7ae509f02b9f01a08564d8da8
iuliarastorgueva <rastorguevaiulia@gmail.com>
2024-01-23 15:50:48 -0600

    first commit

---
+++ src/GroceryList.js
@@ -0,0 +1,67 @@
+import { Component } from 'react';
+import check from './check.png';
+
+export class GroceryList extends Component {
+    state = {
+        userInput: '',
+        groceryList: []
+    }
+
+    onChangeEvent(e) {
+        this.setState({userInput: e});
+    }
+
+    addItem(input) {
+        if (input === '') {
+            alert ("Please enter an item")
+        } else {
+        let listArray = this.state.groceryList;
+        listArray.push(input);
+        this.setState({groceryList: listArray, userInput: ''})
+    }
+    }
+
+    deleteItem() {
+        let listArray = this.state.groceryList;
+        listArray.length = 0;
+        //or listArray = [];
+        this.setState({groceryList: listArray})
+    }
+
+    crossedWord(event) {gi
+        const li = event.target;
+        li.classList.toggle('crossed');
+    }
+
+    onFormSubmit(e) {
+        e.preventDefault();
+    }
+
+    render() {
+        return(
+            <div>
+            <form onSubmit={this.onFormSubmit}>
+            <div className="container">
+                <input type="text"
+                placeholder="What do you want to buy today?"
+                onChange={(e) => {this.onChangeEvent(e.target.value)}}
+                value={this.state.userInput}/>
+            </div>
+            <div className="container">
+                <button onClick={() => this.addItem(this.state.userInput)} className="btn add">Add</button>
+            </div>
+            <ul>
+                {this.state.groceryList.map((item, index) => (
+                    <li onClick={this.crossedWord} key={index}>
+                        <img src={check} width="40px" alt="check-box"/>
+                        {item}</li>
+            ))} 
+            </ul>
+            <div className="container">
+                <button onClick={() => this.state.deleteItem()} className="btn delete">Delete</button>
+            </div>
+            </form>
+            </div>
+        )
+    }
+}
